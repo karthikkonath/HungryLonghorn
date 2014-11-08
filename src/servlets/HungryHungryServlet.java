f4f134d2c0643ade6eb91fc26085f16c67ef6b25
DarkManiel <mark.r.daniel@gmail.com>
2014-11-08 00:44:06 -0600

    Added rest services for Angular calls to json

--- src/servlets/HungryHungryServlet.java
+++ src/servlets/HungryHungryServlet.java
@@ -13,9 +13,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.ListIterator;
- 
-
-
 
 import java.util.Map;
 
@@ -24,12 +21,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
- 
-
-
-
-
-
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -38,79 +29,78 @@ import org.jsoup.select.Elements;
 
 import services.SqlConn;
 
-//Test123
-public class HungryHungryServlet extends HttpServlet{
-	 protected void doGet(HttpServletRequest request,
-	            HttpServletResponse response) throws ServletException, IOException {
-	 
-	      
-	          
-	        //String value = request.getParameter("param1");
-	 
-	 
-//	        response.getWriter().println("<html>");
-//	        response.getWriter().println("<input type=\"text\" name=\"username\">");
-//	        response.getWriter().println("<input type=\"text\" name=\"password\">");
-//	        response.getWriter().println(
-//	                "<input type=\"submit\" value=\"Log in\">Log in </input>");
-//	        response.getWriter().println("</form>");
-//	        response.getWriter().println("</html>");
-	        doPost(request, response);
-	 
-	    }
-	    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+public class HungryHungryServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 
-	        URL url = new URL("http://www.cs.utexas.edu/calendar");
-	          
-	        File datafile = new File("dataFile.txt");
-	        // jsoup test
-	                try {
-	                    Document doc = Jsoup.connect(url.toString()).get();
-	                    Elements links = doc.select(".view-item-calendar");
-	                    ListIterator<Element> iter = links.listIterator();
-	     
-	                    FileOutputStream f = new FileOutputStream(datafile);
-	                    PrintWriter pw = new PrintWriter(f);
-	                    int i = 0;
-	                    SqlConn conn = new SqlConn();
-	                    Map <Integer, ArrayList<String>> map = new HashMap <Integer, ArrayList<String>>();
-	                    while (iter.hasNext()) {
-	                        Element e = (Element) iter.next();
-	                        System.out.println("///////Start//////");
-	                        ArrayList<String> arr = new ArrayList<String>();
-	                        String title = "" + e.select("#node-title").html();
-	                        if (title.contains("Food") || title.contains("Dine") || title.contains("Lunch") || title.contains("Freeze") || title.contains("Tea") || title.contains("TGIF") || title.contains("Tailgate")){
-	                        	arr.add(title);
-		                        System.out.println(title);
-		                        arr.add("" + e.select(".view-data-node-data-field-cal-location-field-cal-location-value").html());
-		                        arr.add("" + e.select(".date-display-start").html() + " - " +e.select(".date-display-end").html());
-		                        map.put(i, arr);
-		                        i ++;
-	                        }
-	                        System.out.println("///////End////////");
-	                        pw.print(e);
-	                    }
-	                    conn.addEvent(map);
-	                    
-	                    System.out.println("len I = " + i);
-	                    pw.flush();
-	                    pw.close();
-	//	                    FileInputStream iStream = new FileInputStream(datafile);
-	//	        			BufferedReader br = new BufferedReader(new InputStreamReader(
-	//	        					iStream));
-	//	        			String line;
-	////	        			//response.getWriter().write("<h2>" + "Links " + "</h2>");
-	////	        			while ((line = br.readLine()) != null) {
-	////	        				if(line.contains("free") || line.contains("TGIF") || line.contains("food")||line.contains("Pizza") || line.contains("Dine"))
-	////	        				response.getWriter().write("<h3>" + line + "</h3>");
-	////	        			}
-	//	        			br.close();
-	    				
-	                     
-	                } catch (Exception exp) {
-	                    response.getWriter().println("<h3>" + "Link not valid. Please enter a valid URL." + "</h3>");
-	                    exp.printStackTrace();
-	                }
-	     
-	    }
+		doPost(request, response);
+
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+
+		URL url = new URL("http://www.cs.utexas.edu/calendar");
+
+		File datafile = new File("dataFile.txt");
+		// jsoup test
+		try {
+			Document doc = Jsoup.connect(url.toString()).get();
+			Elements links = doc.select(".view-item-calendar");
+			ListIterator<Element> iter = links.listIterator();
+
+			FileOutputStream f = new FileOutputStream(datafile);
+			PrintWriter pw = new PrintWriter(f);
+			int i = 0;
+			SqlConn conn = new SqlConn();
+			Map<Integer, ArrayList<String>> map = new HashMap<Integer, ArrayList<String>>();
+			while (iter.hasNext()) {
+				Element e = (Element) iter.next();
+				System.out.println("///////Start//////");
+				ArrayList<String> arr = new ArrayList<String>();
+				String title = "" + e.select("#node-title").html();
+				if (title.contains("Food") || title.contains("Dine")
+						|| title.contains("Lunch") || title.contains("Freeze")
+						|| title.contains("Tea") || title.contains("TGIF")
+						|| title.contains("Tailgate")) {
+					arr.add(title);
+					System.out.println(title);
+					arr.add(""
+							+ e.select(
+									".view-data-node-data-field-cal-location-field-cal-location-value")
+									.html());
+					arr.add("" + e.select(".date-display-start").html() + " - "
+							+ e.select(".date-display-end").html());
+					map.put(i, arr);
+					i++;
+				}
+				System.out.println("///////End////////");
+				pw.print(e);
+			}
+			conn.addEvent(map);
+
+			System.out.println("len I = " + i);
+			pw.flush();
+			pw.close();
+			// FileInputStream iStream = new FileInputStream(datafile);
+			// BufferedReader br = new BufferedReader(new InputStreamReader(
+			// iStream));
+			// String line;
+			// // //response.getWriter().write("<h2>" + "Links " + "</h2>");
+			// // while ((line = br.readLine()) != null) {
+			// // if(line.contains("free") || line.contains("TGIF") ||
+			// line.contains("food")||line.contains("Pizza") ||
+			// line.contains("Dine"))
+			// // response.getWriter().write("<h3>" + line + "</h3>");
+			// // }
+			// br.close();
+
+		} catch (Exception exp) {
+			response.getWriter().println(
+					"<h3>" + "Link not valid. Please enter a valid URL."
+							+ "</h3>");
+			exp.printStackTrace();
+		}
+
+	}
 }
